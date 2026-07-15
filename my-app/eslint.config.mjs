95f51cc118d2457ed6046a11f548efdfaeb9a88e
newagusloya <newagusloya@gmail.com>
2026-07-14 23:09:40 -0700

    chore: add my-app/.next directory to ESLint global ignores

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -9,6 +9,7 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    "my-app/.next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
